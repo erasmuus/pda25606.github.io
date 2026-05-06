3ae98d87b60eadd061bafa7d3d182caa55a3e5d0
pda25606 <eva.esadclm@gmail.com>
2026-05-06 13:23:40 +0300

    Create index.js

---
+++ project./index.js
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <script src="https://aframe.io/releases/1.7.0/aframe.min.js"></script>
+    <style>
+        h1 { text-align: center; }
+        h3 { text-align: center; }
+        a-scene { width: 50%; height: 50%; border: dotted silver 1px; }
+    </style>
+</head>
+
+<body>
+    <h1>ΤΙΤΛΟΣ ΕΡΓΑΣΙΑΣ (Murder mystery)</h1>
+    <h3>Ονοματεπώνυμο (Eva & Eylul)</h3>
+    <hr />
+    <p><strong>Σενάριο (script):</strong>...</p>
+    <hr />
+    <div align="center">
+    <a-scene ondblclick="this.requestFullscreen(https://erasmuus.github.io/pda25606.github.io/project./);" embedded>
+    </a-scene>
+    </div>
+    <hr />
+    <p><strong>Οδηγίες (guidelines):<strong>...</p>
+    <hr />
+    <p><strong>Credits:</strong></p>
+</body>
+
+</html>
